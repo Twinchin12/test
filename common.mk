20120ec68548a4aef2cd0e8148f3571b8512bbc7
HELLBOY017 <abhaygill017@gmail.com>
2023-07-05 09:46:49 +0530

    opkona: Add displayconfig for instantnoodlep

--- common.mk
+++ common.mk
@@ -158,7 +158,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/display/display_id_4630946950956839809.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946950956839809.xml
+    $(LOCAL_PATH)/display/display_id_4630946950956839809.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946950956839809.xml \
+    $(LOCAL_PATH)/display/display_id_4630947194340276609.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947194340276609.xml
 
 # DRM
 PRODUCT_PACKAGES += \
