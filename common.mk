d2d337a3c094f78d0aec30d8e26b6a4e7a54db6e
Abhay Singh Gill <abhaygill017@gmail.com>
2023-07-24 22:24:40 +0530

    opkona: Enable FUSE passthrough
    OnePlus is already giving FUSE passthrough support in their current kernel source let's take advantage of it
    
    Ref:-
    https://source.android.com/docs/core/storage/fuse-passthrough#implementing-fuse-passthrough

--- common.mk
+++ common.mk
@@ -346,6 +346,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
     Launcher3QuickStep
 
+# Use FUSE passthrough
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.fuse.passthrough.enable=true
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
